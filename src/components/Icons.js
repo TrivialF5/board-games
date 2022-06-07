f88a425d13b189037e655ea700bb1dd0035eb4dd
LvL090 <calvod879@gmail.com>
2022-06-07 15:57:36 +0200

    add icons

---
+++ src/components/Icons.js
@@ -0,0 +1,27 @@
+import { GiMusicalNotes } from "react-icons/gi";
+
+<GiMusicalNotes />;
+
+import { IoIosMusicalNote} from "react-icons/io";
+import { IoIosMusicalNotes } from "react-icons/io";
+
+<IoIosMusicalNote />;
+<IoIosMusicalNotes />;
+
+import { MdPiano } from "react-icons/md";
+
+<MdPiano />;
+
+import { GiViolin } from "react-icons/gi";
+
+<GiViolin />;
+
+import { GiHarp} from "react-icons/gi";
+
+<GiHarp />;
+
+import { GiDrumKit} from "react-icons/gi";
+import { GiDrum} from "react-icons/gi";
+
+<GiDrumKit />;
+<GiDrum />;
