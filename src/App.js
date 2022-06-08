0c00fdec8e74f7ee598e5329c9a3c460c62c4f9a
LvL090 <calvod879@gmail.com>
2022-06-08 19:18:46 +0200

    change title and add sound

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Title from "./components/Title"
 import Select from "./views/simon/Select"
 
 export default function App() {
   return (
     <>
-      <header>
-        <h1 className="text-3xl font-bold uppercase text-center">
-          board games
-        </h1>
-      </header>
+    <header>
+      <Title />
+    </header>
       <main>
         <Select />
       </main>
