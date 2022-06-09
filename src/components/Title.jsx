131aea5cb13b4c88e691ea255b02738c6d406974
Ales <aleswebgit@gmail.com>
2022-06-09 10:26:52 +0200

    add use-sound, for simon instrument sound select

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,9 @@
 const Title = () => {
     return (
     <h1 className="text-3xl font-boldn text-center pt-20 pb-20">
-        <p class="text-6xl font-bold">
+        <p className="text-6xl font-bold">
             Simon
-            <span class="text-amber-500 pl-5">
+            <span className="text-amber-500 pl-5">
                 says
             </span>
         </p>
